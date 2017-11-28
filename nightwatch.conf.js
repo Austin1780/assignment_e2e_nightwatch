6080582e1724e91390ef1a357883004f86a0bacf
Austin1780 <austinwilliamsmith@gmail.com>
2017-11-28 16:56:31 -0700

    working on clicking checkbox

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,33 +1,33 @@
-const path = require('path');
-const serverPath = path.resolve('./bin/selenium-server-standalone-3.4.0.jar');
+const path = require("path");
+const serverPath = path.resolve("./bin/selenium-server-standalone-3.4.0.jar");
 
 module.exports = {
-  src_folders: ['tests'],
-  output_folder: 'reports',
+  src_folders: ["tests"],
+  output_folder: "reports",
 
   selenium: {
     start_process: true,
     server_path: serverPath,
     port: 4444,
-    cli_args: {'webdriver.chrome.driver': './bin/chromedriver'},
+    cli_args: { "webdriver.chrome.driver": "./bin/chromedriver.exe" }
   },
 
   test_settings: {
     default: {
-      launch_url: 'https://www.vikingcodeschool.com',
+      launch_url: "https://www.vikingcodeschool.com",
       selenium_port: 4444,
-      selenium_host: 'localhost',
+      selenium_host: "localhost",
       silent: true,
       screenshots: {
         enabled: true,
-        path: 'screenshots',
-      },
+        path: "screenshots"
+      }
     },
 
     chrome: {
       desiredCapabilities: {
-        browserName: 'chrome',
-      },
-    },
-  },
+        browserName: "chrome"
+      }
+    }
+  }
 };
--- tests/test.js
+++ tests/test.js
@@ -1,16 +1,26 @@
 // Your code here ...
+const password = require("../../password");
 
 module.exports = {
-	'vikingTest': browser => {
-		browser
-		.url(browser.launchUrl)
-		.waitForElementVisible('.marketing', 2000)
-		.saveScreenshot('screenshots/homepage.jpg')
-		.click('a[href="/sign_in"]')
-		.waitForElementVisible('.btn', 2000)
-		.saveScreenshot('screenshots/signIn.jpg')
-		.setValue('#user_email', 'tinderholmgene@gmail.com')
-		.setValue(''
-		//.end();
-	}
+  vikingTest: browser => {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible(".marketing", 2000)
+      .saveScreenshot("screenshots/homepage.jpg")
+      .click('a[href="/sign_in"]')
+      .waitForElementVisible(".btn", 2000)
+      .saveScreenshot("screenshots/signIn.jpg")
+      .setValue("#user_email", password["email"])
+      .setValue("#user_password", password["password"])
+      .click("input[type='submit']")
+      .waitForElementVisible(".loki-framework", 2000)
+      .saveScreenshot("screenshots/dashboard.jpg")
+      .url(
+        "http://www.vikingcodeschool.com/dashboard#/testing-javascript/assignment-viking-code-school-exploration"
+      )
+      .click(
+        "img[ng-title='LN01368: Assignment: Viking Code School exploration']"
+      );
+    //.end();
+  }
 };
