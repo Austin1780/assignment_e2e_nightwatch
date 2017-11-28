af8eb581a28e22e430d927de6315210912006618
GeneTinderholm <tinderholmgene@gmail.com>
2017-11-28 16:56:40 -0600

    got to login

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -8,25 +8,26 @@ module.exports = {
   selenium: {
     start_process: true,
     server_path: serverPath,
-    port: 4444
+    port: 4444,
+    cli_args: {'webdriver.chrome.driver': './bin/chromedriver'},
   },
 
   test_settings: {
     default: {
-      launch_url: 'http://localhost:3000',
+      launch_url: 'https://www.vikingcodeschool.com',
       selenium_port: 4444,
       selenium_host: 'localhost',
       silent: true,
       screenshots: {
         enabled: true,
-        path: 'screenshots'
-      }
+        path: 'screenshots',
+      },
     },
 
     chrome: {
       desiredCapabilities: {
-        browserName: 'chrome'
-      }
-    }
-  }
+        browserName: 'chrome',
+      },
+    },
+  },
 };
--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,16 @@
 // Your code here ...
 
-module.exports = {};
+module.exports = {
+	'vikingTest': browser => {
+		browser
+		.url(browser.launchUrl)
+		.waitForElementVisible('.marketing', 2000)
+		.saveScreenshot('screenshots/homepage.jpg')
+		.click('a[href="/sign_in"]')
+		.waitForElementVisible('.btn', 2000)
+		.saveScreenshot('screenshots/signIn.jpg')
+		.setValue('#user_email', 'tinderholmgene@gmail.com')
+		.setValue(''
+		//.end();
+	}
+};
